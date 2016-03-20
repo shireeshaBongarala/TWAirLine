994217f14548cc18118ab9fae68e425884d1599e
vikram K <vikramkb@thoughtworks.com>
2016-03-20 11:49:13 +0530

    [Vik] basic springboot application

---
+++ src/main/java/com/twair/TwAirApplication.java
@@ -0,0 +1,22 @@
+package com.twair;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class TwAirApplication {
+
+	@RequestMapping("/")
+	public String home(Model model) {
+        model.addAttribute("name", "Vikram");
+		return "FlightSearch";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(TwAirApplication.class, args);
+	}
+}
